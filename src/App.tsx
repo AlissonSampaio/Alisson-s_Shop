f46088c2822707f2a8c41931dba2d3b881d50f46
Alisson Alves <alisson2012alves@gmail.com>
2024-08-02 16:48:27 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+
+  return (
+    <div className="flex justify-center items-center align-middle w-full h-96">
+      <span className="text-9xl text-yellow-400">Initial <a href="https://github.com/AlissonSampaio/Alisson-s_Shop" className="cursor-pointer hover:text-red-800">Commit</a> Of Project</span>
+    </div>
+  )
+}
